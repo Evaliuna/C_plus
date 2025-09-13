3da9831e671eb18eb6cbed8e035a4217510e13f9
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2025-09-13 18:12:17 +0600

    sda file of c++ program

--- sda.cpp
+++ sda.cpp
@@ -1,1 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int target;
+    cin>>target;
+    vector<vector<int>> dp(n+1,vector<int>(target+1,0));
+    for(int i=0;i<=n;i++){
+        dp[i][0]=1;
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=target;j++){
+            if(arr[i-1]<=j){
+                dp[i][j]=dp[i-1][j-arr[i-1]] || dp[i-1][j];
+            }
+            else{
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+    }
+    if(dp[n][target]){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
